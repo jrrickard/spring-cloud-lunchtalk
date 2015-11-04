c256252ab2413c973b4693f1faa7c4b0d615613e
“Jeremy <jrickard@vmware.com>
2015-11-04 13:42:06 -0700

    Step 3, add the feign client

--- whiskey-app/WhiskeyOrderService/src/main/java/demo/clients/InventoryService.java
+++ whiskey-app/WhiskeyOrderService/src/main/java/demo/clients/InventoryService.java
@@ -1,9 +1,25 @@
 package demo.clients;
 
+import demo.model.Whiskey;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 //Add Feign Client
+@FeignClient("WhiskeyInventory")
 public interface InventoryService {
 
     //Add interfaces
+    @RequestMapping(value="/whiskey", consumes= MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method= RequestMethod.GET)
+    Iterable<Whiskey> getWhiskey();
 
+    @RequestMapping(value="/whiskey/{uuid}", consumes= MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method=RequestMethod.GET)
+    Whiskey getWhiskey(@PathVariable(value = "uuid") String uuid);
+
+    @RequestMapping(value="/whiskey/{uuid}", consumes= MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method=RequestMethod.PATCH)
+    Whiskey updateWhiskey(@PathVariable(value = "uuid") String uuid, @RequestBody Whiskey whiskey);
 }
 
--- whiskey-app/WhiskeyOrderService/src/main/java/demo/clients/InventoryServiceClientImpl.java
+++ whiskey-app/WhiskeyOrderService/src/main/java/demo/clients/InventoryServiceClientImpl.java
@@ -11,7 +11,24 @@ public class InventoryServiceClientImpl {
 
 
     //Auto Wire the Feign Client
+    @Autowired
+    InventoryService feignClient;
+
     //Implement the contract, hystrix commands
 
+
+    public Iterable<Whiskey> getWhiskey() {
+        return feignClient.getWhiskey();
+    }
+
+
+    public Whiskey getWhiskey(String uuid) {
+        return feignClient.getWhiskey(uuid);
+    }
+
+
+    public Whiskey updateWhiskey( String uuid, Whiskey whiskey) {
+        return feignClient.updateWhiskey(uuid, whiskey);
+    }
 
 }
