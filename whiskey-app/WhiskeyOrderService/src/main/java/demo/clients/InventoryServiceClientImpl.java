ea93bef90bd1e0aa873189a65b8b411aa0735f31
“Jeremy <jrickard@vmware.com>
2015-11-04 13:45:27 -0700

    Adding feign client to controller

--- whiskey-app/WhiskeyOrderService/src/main/java/demo/clients/InventoryServiceClientImpl.java
+++ whiskey-app/WhiskeyOrderService/src/main/java/demo/clients/InventoryServiceClientImpl.java
@@ -10,25 +10,5 @@ import org.springframework.stereotype.Component;
 public class InventoryServiceClientImpl {
 
 
-    //Auto Wire the Feign Client
-    @Autowired
-    InventoryService feignClient;
-
-    //Implement the contract, hystrix commands
-
-
-    public Iterable<Whiskey> getWhiskey() {
-        return feignClient.getWhiskey();
-    }
-
-
-    public Whiskey getWhiskey(String uuid) {
-        return feignClient.getWhiskey(uuid);
-    }
-
-
-    public Whiskey updateWhiskey( String uuid, Whiskey whiskey) {
-        return feignClient.updateWhiskey(uuid, whiskey);
-    }
 
 }
--- whiskey-app/WhiskeyOrderService/src/main/java/demo/controller/WhiskeyOrderController.java
+++ whiskey-app/WhiskeyOrderService/src/main/java/demo/controller/WhiskeyOrderController.java
@@ -1,6 +1,7 @@
 package demo.controller;
 
 
+import demo.clients.InventoryService;
 import demo.clients.InventoryServiceClientImpl;
 import demo.model.Whiskey;
 import demo.model.WhiskeyOrder;
@@ -16,37 +17,43 @@ import java.util.List;
 public class WhiskeyOrderController {
 
 
+    @Autowired
+    WhiskeyOrderRepo repo;
+
+    @Autowired
+    InventoryService client;
 
     @RequestMapping(value="/order", consumes= MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method=RequestMethod.GET)
     public Iterable<WhiskeyOrder> getWhiskey() {
-        return null;
+        return repo.findAll();
     }
 
     @RequestMapping(value="/order/{uuid}", consumes= MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method=RequestMethod.GET)
     public WhiskeyOrder getWhiskey(@PathVariable(value = "uuid") String uuid) {
 
-        return null;
+        return repo.findOne(uuid);
     }
 
     @RequestMapping(value="/order/whiskey/{uuid}", consumes= MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method=RequestMethod.GET)
     public List<WhiskeyOrder> getWhiskeyOrders(@PathVariable(value = "uuid") String uuid) {
-        return null;
+        return repo.findByWhiskeyUUID(uuid);
     }
 
     @RequestMapping(value="/order/", consumes= MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method=RequestMethod.PUT)
     public WhiskeyOrder createWhiskey( @RequestBody WhiskeyOrder order) {
 
         //Get the whiskey
-
+        Whiskey whiskey = client.getWhiskey(order.getWhiskeyUUID());
         //check the inventory
-
+        if (whiskey.getInventory() < order.getNumber()) {
+            throw new IllegalArgumentException("Insufficient inventory remaining");
+        }
         //update the inventory
-
+        whiskey.setInventory(whiskey.getInventory() - order.getNumber());
         //patch the document
-
+        client.updateWhiskey(order.getWhiskeyUUID(), whiskey);
         //save the order
-
-        return null;
+        return repo.save(order);
     }
 
 
